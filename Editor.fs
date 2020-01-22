c9cb633e78f0fb8b730d9521c676f908ede67925
Edney Pitta <edney.pitta@farfetch.com>
2020-01-22 10:10:08 +0000

    Remove useless parenthesis

--- Editor.fs
+++ Editor.fs
@@ -1,9 +1,9 @@
 ﻿module Editor
 
     type Buffer = string list
-    type Cursor = { Row: int; Col: int} 
+    type Cursor = { Row: int; Col: int } 
 
-    let private apply (buffer) (lineIndex) (func) =
+    let private apply buffer lineIndex func =
         buffer |>
         List.indexed |>
         List.map (fun (index, line) ->
@@ -11,14 +11,14 @@
             | i when i = lineIndex -> func line
             | _ -> line)
 
-    let removeChar (buffer: Buffer, cursor) = 
+    let removeChar (buffer: Buffer) cursor = 
         apply buffer cursor.Row (fun line -> line.Remove(cursor.Col, 1))
 
-    let insertChar (buffer: Buffer, cursor, char) =
+    let insertChar (buffer: Buffer) cursor char =
         apply buffer cursor.Row (fun line -> line.Insert(cursor.Col, char))
 
-    let up (cursor) = { cursor with Row = cursor.Row - 1 }
-    let down (cursor) = { cursor with Row = cursor.Row + 1 }
-    let left (cursor) = { cursor with Col = cursor.Col - 1 }
-    let right (cursor) = { cursor with Col = cursor.Col + 1 }
+    let up cursor = { cursor with Row = cursor.Row - 1 }
+    let down cursor = { cursor with Row = cursor.Row + 1 }
+    let left cursor = { cursor with Col = cursor.Col - 1 }
+    let right cursor = { cursor with Col = cursor.Col + 1 }
 
--- Program.fs
+++ Program.fs
@@ -3,32 +3,33 @@ open Editor
 
 type Action = CursorMove | CharInserted | CharDeleted
 
-let render (buffer: Buffer, cursor: Cursor) = 
+let render (buffer: Buffer) cursor = 
     Console.Clear ()
     Console.SetCursorPosition (0, 0)
     for str in buffer do
         Console.WriteLine (str)
-    Console.CursorTop <- cursor.Row
-    Console.CursorLeft <- cursor.Col
+    Console.SetCursorPosition (cursor.Col, cursor.Row)
 
-let handleInput (buffer, cursor) = 
-    let a = Console.ReadKey (true)
-    match a.Key with
+let handleInput buffer cursor = 
+    let keyInfo = Console.ReadKey (true)
+    let char = keyInfo.KeyChar.ToString()
+
+    match keyInfo.Key with
     | ConsoleKey.UpArrow -> (buffer, up cursor, CursorMove)
     | ConsoleKey.DownArrow -> (buffer, down cursor, CursorMove)
     | ConsoleKey.LeftArrow -> (buffer, left cursor, CursorMove)
     | ConsoleKey.RightArrow -> (buffer, right cursor, CursorMove)
-    | ConsoleKey.Backspace -> (removeChar (buffer, cursor), left cursor, CharDeleted)
-    | _ -> (insertChar (buffer, cursor, a.KeyChar.ToString()), right cursor, CharInserted)
+    | ConsoleKey.Backspace -> (removeChar buffer cursor, left cursor, CharDeleted)
+    | _ -> (insertChar buffer cursor char, right cursor, CharInserted)
 
 [<EntryPoint>]
 let main argv =
     let mutable buffer = ["first"; "second"; "second"; "second"; "second"]
     let mutable cursor = { Row = 1; Col = 2 }
 
-    render (buffer, cursor)
+    render buffer cursor
     while true do
-        let (newBuffer, newCursor, action) = handleInput(buffer, cursor)
+        let (newBuffer, newCursor, action) = handleInput buffer cursor
         buffer <- newBuffer
         cursor <- newCursor
 
