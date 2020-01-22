afb60619659a37a22f1472f5738b4305a30d73e8
Edney Pitta <edney.pitta@farfetch.com>
2020-01-22 18:53:23 +0000

    Make Buffer a single case discriminated union

--- Editor.fs
+++ Editor.fs
@@ -1,21 +1,24 @@
 ﻿module Editor
 
-    type Buffer = string list
+    type Buffer = Buffer of string list
     type Cursor = { Row: int; Col: int } 
 
     let private apply buffer lineIndex func =
-        buffer |>
+        let (Buffer lines) = buffer
+
+        lines |>
         List.indexed |>
         List.map (fun (index, line) ->
             match index with
             | i when i = lineIndex -> func line
-            | _ -> line)
+            | _ -> line) |>
+        Buffer
 
-    let removeChar (buffer: Buffer) cursor = 
+    let removeChar buffer cursor = 
         apply buffer cursor.Row (fun line -> line.Remove(cursor.Col, 1))
 
-    let insertChar (buffer: Buffer) cursor char =
-        apply buffer cursor.Row (fun line -> line.Insert(cursor.Col, char))
+    let insertChar buffer cursor char =
+        apply buffer cursor.Row (fun line -> line.Insert(cursor.Col, char)) 
 
     let up cursor = { cursor with Row = cursor.Row - 1 }
     let down cursor = { cursor with Row = cursor.Row + 1 }
--- Program.fs
+++ Program.fs
@@ -4,13 +4,14 @@ open Editor
 type Action = CursorMove | CharInserted | CharDeleted
 
 let render (buffer: Buffer) cursor = 
+    let (Buffer lines) = buffer
     Console.Clear ()
     Console.SetCursorPosition (0, 0)
-    for str in buffer do
+    for str in lines do
         Console.WriteLine (str)
     Console.SetCursorPosition (cursor.Col, cursor.Row)
 
-let handleInput buffer cursor = 
+let handleInput (buffer: Buffer) cursor = 
     let keyInfo = Console.ReadKey (true)
     let char = keyInfo.KeyChar.ToString()
 
@@ -23,11 +24,12 @@ let handleInput buffer cursor =
     | _ -> (insertChar buffer cursor char, right cursor, CharInserted)
 
 [<EntryPoint>]
-let main argv =
-    let mutable buffer = ["first"; "second"; "second"; "second"; "second"]
+let main _ =
+    let mutable buffer = Buffer ["first"; "second"; "second"; "second"; "second"]
     let mutable cursor = { Row = 1; Col = 2 }
 
     render buffer cursor
+
     while true do
         let (newBuffer, newCursor, action) = handleInput buffer cursor
         buffer <- newBuffer
@@ -39,6 +41,6 @@ let main argv =
             Console.SetCursorPosition (0, newCursor.Row)
             Console.Write(new string(' ', Console.BufferWidth)); 
             Console.SetCursorPosition (0, newCursor.Row)
-            Console.Write (newBuffer.[newCursor.Row])
+            Console.Write (match newBuffer with Buffer b -> b.[newCursor.Row])
             Console.SetCursorPosition (newCursor.Col, newCursor.Row)
-    0 // return an integer exit code
+    0
