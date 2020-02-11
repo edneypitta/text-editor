09ab62e57ed6f9ad841450dd0c5c8af9fa9260d9
Edney Pitta <edney.pitta@farfetch.com>
2020-02-11 18:37:22 +0000

    indent code

--- text-editor/Editor.fs
+++ text-editor/Editor.fs
@@ -1,40 +1,55 @@
 ﻿module Editor
 
-    type Buffer = Buffer of string list
-    type Cursor = { Row: int; Col: int } 
+  type Buffer = string list
+  type Cursor = { Row: int; Col: int }
 
-    let private apply (Buffer lines) lineIndex func =
-        lines |>
-        List.indexed |>
-        List.map (fun (index, line) ->
-            match index with
-            | i when i = lineIndex -> func line
-            | _ -> line) |>
-        Buffer
+  type Editor = {
+    Cursor: Cursor
+    Buffer: Buffer
+  }
 
-    let removeChar buffer cursor = 
-        apply buffer cursor.Row (fun line -> line.Remove(cursor.Col, 1))
+  let private apply editor func =
+    editor.Buffer |>
+    List.indexed |>
+    List.map (fun (index, line) ->
+      match index with
+      | i when i = editor.Cursor.Row -> func line
+      | _ -> line)
+      
+  let private splitLine editor =
+    editor.Buffer |>
+    List.indexed |>
+    List.fold (fun acc elem -> 
+               acc @
+               match elem with
+               | (index, line) when index = editor.Cursor.Row -> 
+                 [
+                   line.Substring(0, editor.Cursor.Col)
+                   line.Substring(editor.Cursor.Col, line.Length - line.Substring(0, editor.Cursor.Col).Length)
+                 ]
+               | (_, line) -> [line]) []
 
-    let insertChar buffer cursor char =
-        apply buffer cursor.Row (fun line -> line.Insert(cursor.Col, char)) 
-        
-    let splitLine (Buffer lines) cursor = 
-        lines |>
-        List.indexed |>
-        List.fold (fun acc elem -> 
-                   acc @
-                   match elem with
-                   | (index, line) when index = cursor.Row -> 
-                        [
-                            line.Substring(0, cursor.Col)
-                            line.Substring(cursor.Col, line.Length - line.Substring(0, cursor.Col).Length)
-                        ]
-                   | (_, line) -> [line]) [] |>
-        Buffer
+  let up cursor = { cursor with Row = cursor.Row - 1 }
+  let down cursor = { cursor with Row = cursor.Row + 1 }
+  let left cursor = { cursor with Col = cursor.Col - 1 }
+  let right cursor = { cursor with Col = cursor.Col + 1 }
+  let leftMost cursor = { cursor with Col = 0 }
 
-    let up cursor = { cursor with Row = cursor.Row - 1 }
-    let down cursor = { cursor with Row = cursor.Row + 1 }
-    let left cursor = { cursor with Col = cursor.Col - 1 }
-    let right cursor = { cursor with Col = cursor.Col + 1 }
-    let leftMost cursor = { cursor with Col = 0 }
+  let removeChar editor = 
+    {
+      Cursor = editor.Cursor |> down |> leftMost
+      Buffer = apply editor (fun line -> line.Remove(editor.Cursor.Col, 1))
+    }
+  
+  let insertChar editor char =
+    {
+      Cursor = editor.Cursor |> right
+      Buffer = apply editor (fun line -> line.Insert(editor.Cursor.Col, char)) 
+    }
+
+  let enter editor =
+    {
+      Cursor = editor.Cursor |> down |> leftMost
+      Buffer = splitLine editor
+    }
 
--- text-editor/Program.fs
+++ text-editor/Program.fs
@@ -3,52 +3,54 @@ open Editor
 
 type Action = CursorMove | CharInserted | CharDeleted | Enter
 
-let render (Buffer lines) cursor = 
-    Console.Clear ()
-    Console.SetCursorPosition (0, 0)
-    for str in lines do
-        Console.WriteLine (str)
-    Console.SetCursorPosition (cursor.Col, cursor.Row)
+let render editor = 
+  Console.Clear ()
+  Console.SetCursorPosition (0, 0)
+  for str in editor.Buffer do
+      Console.WriteLine (str)
+  Console.SetCursorPosition (editor.Cursor.Col, editor.Cursor.Row)
 
-let handleInput buffer cursor = 
-    let keyInfo = Console.ReadKey (true)
-    let char = keyInfo.KeyChar.ToString()
+let handleInput editor = 
+  let keyInfo = Console.ReadKey (true)
+  let char = keyInfo.KeyChar.ToString()
 
-    match keyInfo.Key with
-    | ConsoleKey.UpArrow -> (buffer, up cursor, CursorMove)
-    | ConsoleKey.DownArrow -> (buffer, down cursor, CursorMove)
-    | ConsoleKey.LeftArrow -> (buffer, left cursor, CursorMove)
-    | ConsoleKey.RightArrow -> (buffer, right cursor, CursorMove)
-    | ConsoleKey.Backspace -> (removeChar buffer cursor, left cursor, CharDeleted)
-    | ConsoleKey.Enter -> (splitLine buffer cursor, cursor |> down |> leftMost, Enter)
-    | _ -> (insertChar buffer cursor char, right cursor, CharInserted)
+  match keyInfo.Key with
+  | ConsoleKey.UpArrow -> ({ editor with Cursor = up editor.Cursor }, CursorMove)
+  | ConsoleKey.DownArrow -> ({ editor with Cursor = down editor.Cursor }, CursorMove)
+  | ConsoleKey.LeftArrow -> ({ editor with Cursor = left editor.Cursor }, CursorMove)
+  | ConsoleKey.RightArrow -> ({ editor with Cursor = right editor.Cursor }, CursorMove)
+  | ConsoleKey.Backspace -> (removeChar editor, CharDeleted)
+  | ConsoleKey.Enter -> (enter editor, Enter)
+  | _ -> (insertChar editor char, CharInserted)
 
-let writeRow (Buffer lines) row =
-    Console.SetCursorPosition (0, row)
-    Console.Write(new string(' ', Console.BufferWidth)); 
-    Console.SetCursorPosition (0, row)
-    Console.Write (lines.[row])
+let writeRow (lines: Buffer) row =
+  Console.SetCursorPosition (0, row)
+  Console.Write (new string(' ', Console.BufferWidth)); 
+  Console.SetCursorPosition (0, row)
+  Console.Write (lines.[row])
 
 [<EntryPoint>]
 let main _ =
-    let mutable buffer = Buffer [ "the quick brown fox"; "jumped over the lazy dog" ]
-    let mutable cursor = { Row = 0; Col = 7 }
+  let mutable editor = {
+    Buffer = ["the quick brown fox"; "jumped over the lazy dog"]
+    Cursor = { Row = 0; Col = 7 }
+  }
 
-    render buffer cursor
+  render editor
 
-    while true do
-        let (newBuffer, newCursor, action) = handleInput buffer cursor
-        buffer <- newBuffer
-        cursor <- newCursor
+  while true do
+    let (newEditor, action) = handleInput editor
+    editor <- newEditor
 
-        match action with
-        | CharInserted | CharDeleted -> 
-            writeRow buffer cursor.Row
-        | Enter -> 
-            let (Buffer lines) = buffer
-            for row in [(cursor.Row - 1)..lines.Length - 1] do
-                writeRow buffer row
-        | _ -> ()
+    match action with
+    | CharInserted | CharDeleted -> 
+        writeRow editor.Buffer editor.Cursor.Row
+    | Enter ->
+        let previousRow = editor.Cursor.Row - 1
+        let lastRow = editor.Buffer.Length - 1
+        for row in [previousRow..lastRow] do
+            writeRow editor.Buffer row
+    | _ -> ()
 
-        Console.SetCursorPosition (cursor.Col, cursor.Row)
-    0
+    Console.SetCursorPosition (editor.Cursor.Col, editor.Cursor.Row)
+  0
